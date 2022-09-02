422e7e3a6e7d95dba8e54dfc2044a746f7a7530b
sahanashetty31 <30534288+sahanashetty31@users.noreply.github.com>
2022-09-02 23:10:30 +0530

    day2 files

---
+++ src/week1/day2/src/datatype/Area.java
@@ -0,0 +1,11 @@
+package datatype;
+
+public class Area {
+    public static void main(String[] args) {
+        double pi, r, a;
+        r = 10.8; //radius of circle
+        pi = 3.1416; //pi, approx
+        a = pi * r * r; //compute area
+        System.out.print("Area of circle is "+a);
+    }
+}
