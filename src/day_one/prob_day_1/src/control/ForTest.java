536f007538006f82cdee99db1212bcbe68dcf166
sahanashetty31 <30534288+sahanashetty31@users.noreply.github.com>
2022-09-02 20:13:26 +0530

    added day2 files

--- src/day_one/prob_day_1/src/control/ForTest.java
+++
@@ -1,9 +0,0 @@
-package control;
-
-public class ForTest {
-    public static void main(String[] args) {
-        int x;
-        for(x=0; x<10; x=x+1)
-            System.out.println("This is x: "+x);
-    }
-}
