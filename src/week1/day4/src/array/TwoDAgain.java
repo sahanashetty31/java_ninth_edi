0ad33099c9accbb0a8564d38fda7c51608e5d7e7
sahanashetty31 <30534288+sahanashetty31@users.noreply.github.com>
2022-09-04 17:14:59 +0530

    day4

---
+++ src/week1/day4/src/array/TwoDAgain.java
@@ -0,0 +1,22 @@
+package array;
+
+public class TwoDAgain {
+    public static void main(String[] args) {
+        int twoA[][] = new int[4][];
+        twoA[0] = new int[1];
+        twoA[1] = new int[2];
+        twoA[2] = new int[3];
+        twoA[3] = new int[4];
+        int i, j, k=0;
+        for(i=0;i<4;i++)
+            for(j=0;j<i+1;j++){
+                twoA[i]][j] = k;
+                k++;
+            }
+        for(i=0;i<4;i++){
+            for(j=0;j<i+j++)
+                System.out.println(twoA[i][j] + " ");
+            System.out.println();
+        }
+    }
+}
