ed01084e485fdd7cc0269186e3450a177d59469c
sahanashetty31 <30534288+sahanashetty31@users.noreply.github.com>
2022-09-07 12:46:01 +0530

    week2

---
+++ src/week2/day1/src/operators/Modulus.java
@@ -0,0 +1,10 @@
+package operators;
+
+public class Modulus {
+    public static void main(String[] args) {
+        int x = 42;
+        double y = 42.5;
+        System.out.println("x mod 10 = " + x % 10);
+        System.out.println("y mod 10 = " + y % 10);
+    }
+}
