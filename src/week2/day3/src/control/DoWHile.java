30941835a49a6053bdaf78b28912e858f3d8a193
sahanashetty31 <30534288+sahanashetty31@users.noreply.github.com>
2022-09-09 18:00:12 +0530

    w2d3 pgms

---
+++ src/week2/day3/src/control/DoWHile.java
@@ -0,0 +1,11 @@
+package control;
+
+public class DoWHile {
+    public static void main(String[] args) {
+        int n = 10;
+        do{
+            System.out.println("tick "+n);
+            n--;
+        }while(n > 0);
+    }
+}
