6a2f104384ca100be2d6d77b582d51e8da334fa1
sahanashetty31 <30534288+sahanashetty31@users.noreply.github.com>
2022-11-11 07:40:16 +0530

    throw,finally

---
+++ src/week8/src/nov11/src/FinallyDemo.java
@@ -0,0 +1,40 @@
+class FinallyDemo {
+    //throw an exception out of the method
+    static void procA(){
+        try{
+            System.out.println("inside procA");
+            throw new RuntimeException("demo");
+        }finally {
+            System.out.println("procA's finally");
+        }
+    }
+
+    //return from within a try block
+    static void procB(){
+        try {
+            System.out.println("inside procB");
+            return;
+        }finally {
+            System.out.println("procB's finally");
+        }
+    }
+
+    //execute a try block finally
+    static void procC(){
+        try {
+            System.out.println("inside procC");
+        }finally {
+            System.out.println("procC's finally");
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            procA();
+        }catch (Exception e){
+            System.out.println("Exception caught");
+        }
+        procB();
+        procC();
+    }
+}
